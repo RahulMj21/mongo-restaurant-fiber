d422fa1732b0eca890f854f188f3214c3f8d5c5b
Rahul Mondal <rahulmondar@gmail.com>
2023-02-04 11:18:23 +0530

    feat: added logger

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/RahulMj21/mongo-restaurant-fiber/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
 	api := app.Group("/api/v1")
 
 	routes.TestRoutes(api)
